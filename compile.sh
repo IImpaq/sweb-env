0b82796def3563276df1475a5d68c2acf16d1b9a
Marcus Gugacs <iimpaq@proton.me>
2023-02-20 16:49:47 +0100

    Added debug functionality

--- compile.sh
+++ compile.sh
@@ -1,3 +1,17 @@
 #!/bin/zsh
 
-docker exec -it --workdir /tmp os-ubuntu bash -c "cd sweb/ && cmake -GNinja ../src/ && ninja"
+mode=$1
+
+cd /tmp/sweb/ || exit 1
+
+if [[ -n "$mode" ]]; then
+  if [[ "$mode" == "release" ]]; then
+    docker exec -it --workdir /tmp os-ubuntu bash -c "cd sweb/ && cmake -DARCH=x86/64 -GNinja ../src/ && ninja"
+  elif [[ "$mode" == "debug" ]]; then
+    docker exec -it --workdir /tmp os-ubuntu bash -c "cd sweb/ && cmake -DDEBUG=1 -DARCH=x86/64 -GNinja ../src/ && ninja"
+  else
+    echo "no valid mode passed (options: release/debug)"
+  fi
+else
+  echo "no mode passed (valid options: release/debug)"
+fi
--- emulate.sh
+++ emulate.sh
@@ -1,3 +1,19 @@
 #!/bin/zsh
 
-qemu-system-x86_64 /tmp/sweb/SWEB.qcow2
+mode=$1
+
+cd /tmp/sweb/ || exit 1
+
+if [[ -n "$mode" ]]; then
+  if [[ "$mode" == "run" ]]; then
+    qemu-system-x86_64 -m 8M -drive file=SWEB.qcow2,index=0,media=disk -debugcon stdio -no-reboot -cpu qemu64 | tee output.log
+  elif [[ "$mode" == "debug" ]]; then
+    echo "make sure to compile in debug mode before"
+    qemu-system-x86_64 -m 8M -drive file=SWEB.qcow2,index=0,media=disk -debugcon stdio -no-reboot -s -S | tee output.log
+  else
+    echo "no valid mode passed (options: run/debug)"
+  fi
+else
+  echo "no mode passed (valid options: run/debug)"
+fi
+
